3679a0983974d006f8687da476a1c98da8f1062d
Taha Elmokadem <eng.taha.k@gmail.com>
2019-08-04 20:59:43 +1000

    tuned

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1656,7 +1656,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("GLOBAL_POSITION_INT", 5.0f);
 		configure_stream_local("GPS2_RAW", unlimited_rate);
 		configure_stream_local("GPS_RAW_INT", unlimited_rate);
-		configure_stream_local("HIGHRES_IMU", 120.0f);
+		configure_stream_local("HIGHRES_IMU", 100.0f);
 		configure_stream_local("HOME_POSITION", 0.5f);
 		configure_stream_local("LOCAL_POSITION_NED", 3.0f);
 		configure_stream_local("NAMED_VALUE_FLOAT", 10.0f);
